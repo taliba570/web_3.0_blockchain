2278a621eaafde034093d31cf66531ba44914448
Talib Allauddin <taliballauddin@gmail.com>
2022-08-20 10:44:09 +0500

    init

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar, Welcome, Footer, Services, Transactions } from './components'
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>s
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
